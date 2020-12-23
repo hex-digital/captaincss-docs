4dc0eb134af1b4d33cb8ee4932327d01b4c114fb
Jamie Warburton <jamie@hexdigital.com>
2020-12-23 22:19:00 +0000

    feat: add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,6 +20,7 @@ export default class Document extends NextDocument {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />
+          <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
